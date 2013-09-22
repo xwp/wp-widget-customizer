2ead257f7d630bead0922b5b478ae9503f6f0914
Weston Ruter <weston@x-team.com>
2013-09-22 00:25:13 -0700

    Register widget click live at $.ready

--- widget-customizer-preview.js
+++ widget-customizer-preview.js
@@ -3,28 +3,39 @@ var WidgetCustomizerPreview = (function ($) {
 	'use strict';
 
 	var self = {
-		rendered_sidebars: []
+		rendered_sidebars: [],
+
+		init: function () {
+			this.toggleSections();
+			this.highlightControls();
+		},
+
+		toggleSections: function () {
+			parent.jQuery('.control-section[id^="accordion-section-sidebar-widgets-"]').hide();
+			$.each( self.rendered_sidebars, function ( i, sidebar_id ) {
+				parent.jQuery('#accordion-section-sidebar-widgets-' + sidebar_id).show();
+			});
+		},
+
+		highlightControls: function() {
+			$(document).on( 'click', '.widget', function () {
+				var widget_id = '#customize-control-widget_' + $(this).attr('id');
+				var widget = parent.jQuery(widget_id).children('div.widget');
+				var inside = widget.children('.widget-inside');
+
+				if ( inside.is(':hidden') ) {
+					inside.slideDown('fast');
+				} else {
+					inside.slideUp('fast', function () {
+						widget.css({'width':'', margin:''});
+					});
+				}
+			});
+		}
 	};
 
 	$(function () {
-		parent.jQuery('.control-section[id^="accordion-section-sidebar-widgets-"]').hide();
-		$.each( self.rendered_sidebars, function ( i, sidebar_id ) {
-			parent.jQuery('#accordion-section-sidebar-widgets-' + sidebar_id).show();
-		});
-	});
-
-	$('.widget').click(function () {
-		var widgetId = '#customize-control-widget_' + $(this).attr('id');
-		var widget = parent.jQuery(widgetId).children('div.widget');
-		var inside = widget.children('.widget-inside');
-
-		if ( inside.is(':hidden') ) {
-			inside.slideDown('fast');
-		} else {
-			inside.slideUp('fast', function () {
-				widget.css({'width':'', margin:''});
-			});
-		}
+		self.init();
 	});
 
 	return self;
