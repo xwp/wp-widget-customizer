3535f5105fdbd92f66492d15e6ebaacd4bb9452c
Weston Ruter <weston@x-team.com>
2013-10-15 21:38:08 -0700

    Update readme for 0.8

--- widget-customizer.php
+++ widget-customizer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Widget Customizer
  * Description: Edit widgets and preview changes in Theme Customizer, with a control for each widget form in sections added for each sidebar rendered in the preview.
- * Version:     0.7
+ * Version:     0.8
  * Author:      X-Team
  * Author URI:  http://x-team.com/wordpress/
  * License:     GPLv2+
