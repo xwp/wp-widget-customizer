75dbd04326d69e3e245396959a36e7737dbfd2b0
Weston Ruter <weston@x-team.com>
2013-12-08 00:52:13 -0800

    Simplify conditions for twentythirteen theme support JS

--- theme-support/twentythirteen.js
+++ theme-support/twentythirteen.js
@@ -1,11 +1,7 @@
 /*global jQuery, wp */
 jQuery( function ($) {
 	wp.customize.bind( 'sidebar-updated', function ( sidebar_id ) {
-		if ( 'sidebar-1' !== sidebar_id ) {
-			return;
-		}
-
-		if ( $.isFunction( $.fn.masonry ) ) {
+		if ( 'sidebar-1' === sidebar_id && $.isFunction( $.fn.masonry ) ) {
 			var widget_area = $( '#secondary .widget-area' );
 			widget_area.masonry( 'reloadItems' );
 			widget_area.masonry();
